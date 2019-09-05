2e4c69ea059c8e1a86f8e606da27d8e4609d5d6b
Adrian T. Saycon <adzbite@gmail.com>
2019-09-05 14:36:58 +0800

    asset ver 5.1

--- functions.php
+++ functions.php
@@ -20,10 +20,10 @@ register_deactivation_hook( __FILE__, 'pluginprefix_deactivation' );
 function enqueueAssets()
 {
   wp_enqueue_style( 'sloBootstrapCss', plugins_url('src/assets/node_modules/bootstrap/dist/css/bootstrap-grid.min.css',__FILE__) ,false, '1.1', 'all');
-  wp_enqueue_style( 'sloMainStyles', plugins_url('src/assets/css/styles.css',__FILE__) ,false, null, 'all');
+  wp_enqueue_style( 'sloMainStyles', plugins_url('src/assets/css/styles.css',__FILE__) ,false, 5.1, 'all');
   wp_enqueue_script( 'momentJs', plugins_url('src/assets/node_modules/moment/moment.js', __FILE__), null, null, false);
   wp_enqueue_script( 'momentTimezone', plugins_url('src/assets/node_modules//moment-timezone/moment-timezone.js', __FILE__), 'momentJs', null, false);
-  wp_enqueue_script( 'sloJs', plugins_url('src/assets/js/main.js', __FILE__), ['jquery'], null, false);
+  wp_enqueue_script( 'sloJs', plugins_url('src/assets/js/main.js', __FILE__), ['jquery'], 5.1, false);
   wp_localize_script('sloJs', 'sloData', array(
     'pluginsUrl' => plugins_url(),
   ));
