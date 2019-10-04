037dc3a1fcdaaf0d0ad739bb4abc54b06cfeab02
Adrian T. Saycon <adzbite@gmail.com>
2019-10-04 14:22:40 +0800

    fix...

--- functions.php
+++ functions.php
@@ -22,7 +22,7 @@ function enqueueAssets()
   wp_enqueue_style( 'sloMainStyles', plugins_url('src/assets/css/styles.css',__FILE__) ,false, '6.1.9', 'all');
   wp_enqueue_script( 'momentJs', plugins_url('src/assets/node_modules/moment/moment.js', __FILE__), null, null, false);
   wp_enqueue_script( 'momentTimezone', plugins_url('src/assets/node_modules//moment-timezone/moment-timezone.js', __FILE__), 'momentJs', null, false);
-  wp_enqueue_script( 'sloJs', plugins_url('src/assets/js/main.js', __FILE__), ['jquery'], '6.3.7', false);
+  wp_enqueue_script( 'sloJs', plugins_url('src/assets/js/main.js', __FILE__), ['jquery'], '6.3.9', false);
   wp_localize_script('sloJs', 'sloData', array(
     'pluginsUrl' => plugins_url(),
   ));
